753d67f4a6f22a5a707dc59a4e08e663a3f75825
Samiullah Mahmodi <113135377+sami-mahmodi@users.noreply.github.com>
2023-02-16 22:41:30 +0500

    posts and logo added

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import classes from "./navigation.module.css";
+import Logo from "./logo";
+export default function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ components/posts/post-grid.js
@@ -0,0 +1,13 @@
+import PostItem from "./post-item";
+import classes from "./post-grid.module.css";
+
+export default function PostGrid(props) {
+  const { posts } = props;
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem />
+      ))}
+    </ul>
+  );
+}
